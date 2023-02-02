19fac56d8c9539b7fb3f3d161ab473dc693f6f36
benshidanny11 <benshidanny11@gmail.com>
2023-02-02 11:48:40 +0200

    Login admin

--- src/routes/RequireAuthAdmin.jsx
+++ src/routes/RequireAuthAdmin.jsx
@@ -6,7 +6,7 @@ function RequireAuthAdmin() {
   return (
     auth?.access_token && auth?.userData?.u_role === 'SUPER_ADMIN'
       ? <Outlet />
-      : <Navigate to="/login" />
+      : <Navigate to="/adminlogin" />
   );
 }
 
